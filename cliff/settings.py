ae098415edbd456f670d0118283f83197855511b
Josh Ourisman <josh@joshourisman.com>
2014-04-16 15:34:30 -0400

    Set target="_blank" in the link to the Django docs.

--- cliff/settings.py
+++ cliff/settings.py
@@ -4,6 +4,7 @@ DJANGO_VERSION = get_version()
 MAJOR_MINOR_VERSION = '.'.join(DJANGO_VERSION.split('.')[0:2])
 TEMPLATE_FIELD_HELP_TEXT = """
 This field uses the Django template language:
-<a href="https://docs.djangoproject.com/en/{version}/topics/templates/">
-https://docs.djangoproject.com/en/{version}/topics/templates/</a>""".format(
+<a href="https://docs.djangoproject.com/en/{version}/topics/templates/"
+target="_blank"> https://docs.djangoproject.com/en/{version}/topics/templates/
+</a>""".format(
     version=MAJOR_MINOR_VERSION)
