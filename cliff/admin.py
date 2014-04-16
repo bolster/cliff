265a4132337e13221e7717a261714ef7e24662e8
Josh Ourisman <josh@joshourisman.com>
2014-04-16 15:21:44 -0400

    Added help_text to EmailTemplateForm.subject, because the custom field crushes the model's help_text.

--- cliff/admin.py
+++ cliff/admin.py
@@ -6,7 +6,11 @@ from .models import EmailTemplate
 
 class EmailTemplateForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'size': '30'}))
-    subject = forms.CharField(widget=forms.TextInput(attrs={'size': '75'}))
+    subject = forms.CharField(
+        widget=forms.TextInput(attrs={'size': '75'}), help_text="""
+This field uses the Django template language:
+<a href="https://docs.djangoproject.com/en/1.5/topics/templates/">
+https://docs.djangoproject.com/en/1.5/topics/templates/</a>""")
 
     class Meta:
         model = EmailTemplate
