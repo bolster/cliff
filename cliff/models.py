8c4d769630210e02eddf49bf5f09a60c3315e822
Josh Ourisman <josh@joshourisman.com>
2014-04-16 15:19:39 -0400

    Added links to the Django template language docs to the help_text for EmailTemplate.subject, EmailTemplate.body_html, and EmailTemplate.body_plain.

--- cliff/models.py
+++ cliff/models.py
@@ -3,9 +3,18 @@ from django.db import models
 
 class EmailTemplate(models.Model):
     name = models.TextField()
-    subject = models.TextField()
-    body_html = models.TextField()
-    body_plain = models.TextField()
+    subject = models.TextField(help_text="""
+This field uses the Django template language:
+<a href="https://docs.djangoproject.com/en/1.5/topics/templates/">
+https://docs.djangoproject.com/en/1.5/topics/templates/</a>""")
+    body_html = models.TextField(help_text="""
+This field uses the Django template language:
+<a href="https://docs.djangoproject.com/en/1.5/topics/templates/">
+https://docs.djangoproject.com/en/1.5/topics/templates/</a>""")
+    body_plain = models.TextField(help_text="""
+This field uses the Django template language:
+<a href="https://docs.djangoproject.com/en/1.5/topics/templates/">
+https://docs.djangoproject.com/en/1.5/topics/templates/</a>""")
 
     def __unicode__(self):
         return self.name
