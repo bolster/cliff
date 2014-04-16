c924356ee844053dd5b9219185b3fc6900cbcccd
Josh Ourisman <josh@joshourisman.com>
2014-04-16 16:01:17 -0400

    Using custom css to improve display of ACE Editor.

--- cliff/admin.py
+++ cliff/admin.py
@@ -1,14 +1,29 @@
 from django import forms
 from django.contrib import admin
+from django_ace import AceWidget
 
 from .models import EmailTemplate
 from .settings import TEMPLATE_FIELD_HELP_TEXT
+
+
+class CliffAceWidget(AceWidget):
+
+    class Media:
+        css = {
+            'all': ('/static/cliff/emailtemplateform.css',)
+        }
 
 
 class EmailTemplateForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'size': '30'}))
     subject = forms.CharField(
         widget=forms.TextInput(attrs={'size': '75'}),
+        help_text=TEMPLATE_FIELD_HELP_TEXT)
+    body_html = forms.CharField(
+        widget=CliffAceWidget(mode='django'),
+        help_text=TEMPLATE_FIELD_HELP_TEXT)
+    body_plain = forms.CharField(
+        widget=CliffAceWidget(mode='django'),
         help_text=TEMPLATE_FIELD_HELP_TEXT)
 
     class Meta:
