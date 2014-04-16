99d1a18533e82eff2b1a484de2cfd30d793b6683
Josh Ourisman <josh@joshourisman.com>
2014-04-16 15:08:55 -0400

    Overriding form for EmailTemplates in admin for better name and subject inputs.

--- cliff/admin.py
+++ cliff/admin.py
@@ -1,5 +1,18 @@
+from django import forms
 from django.contrib import admin
 
 from .models import EmailTemplate
 
-admin.site.register(EmailTemplate)
+
+class EmailTemplateForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'size': '30'}))
+    subject = forms.CharField(widget=forms.TextInput(attrs={'size': '75'}))
+
+    class Meta:
+        model = EmailTemplate
+
+
+class EmailTemplateAdmin(admin.ModelAdmin):
+    form = EmailTemplateForm
+
+admin.site.register(EmailTemplate, EmailTemplateAdmin)
