8f11c62ccad1bd90895c909ecb8bdceaa3b751ea
Josh Ourisman <josh@joshourisman.com>
2014-04-16 15:32:39 -0400

    Link to the docs for the installed version of Django.

--- cliff/settings.py
+++ cliff/settings.py
@@ -1,4 +1,9 @@
+from django import get_version
+
+DJANGO_VERSION = get_version()
+MAJOR_MINOR_VERSION = '.'.join(DJANGO_VERSION.split('.')[0:2])
 TEMPLATE_FIELD_HELP_TEXT = """
 This field uses the Django template language:
-<a href="https://docs.djangoproject.com/en/1.5/topics/templates/">
-https://docs.djangoproject.com/en/1.5/topics/templates/</a>"""
+<a href="https://docs.djangoproject.com/en/{version}/topics/templates/">
+https://docs.djangoproject.com/en/{version}/topics/templates/</a>""".format(
+    version=MAJOR_MINOR_VERSION)
